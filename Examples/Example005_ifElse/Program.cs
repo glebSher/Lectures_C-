7727a95f30d59452c44b1a64c9f929b3fa8b5bac
glebshcherbakov <gleb.sher309@gmail.com>
2022-09-06 09:11:31 +0300

    ifElse

---
+++ Examples/Example005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "глеб")
+{
+    Console.WriteLine("Ура, это же Глеб!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
